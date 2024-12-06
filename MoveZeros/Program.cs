8a1c4b2f371127283b1f913eb3f1c3447b5ccb96
Class_Student <Class_Student@COPM-4-6-20>
2024-12-06 09:21:21 +0300

    Добавьте файлы проекта.

---
+++ MoveZeros/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        int[] nums = new int[input.Length];
+        for (int i = 0; i < input.Length; i++)
+        {
+            nums[i] = int.Parse(input[i]);
+        }
+
+      
+        int nonZeroIndex = 0;
+
+       
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                int temp = nums[nonZeroIndex];
+                nums[nonZeroIndex] = nums[i];
+                nums[i] = temp;
+                nonZeroIndex++;
+            }
+        }
+
+        
+        Console.WriteLine(string.Join(",", nums));
+    }
+}
